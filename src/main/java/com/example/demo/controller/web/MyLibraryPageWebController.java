6a6c5062c6bca0f05236b7d4b2a60ca4555c2322
Azazagich <s.kovalchuk@innovinnprom.com>
2025-03-07 14:39:43 +0200

    feat(web-controller): add web controller for pages

---
+++ src/main/java/com/example/demo/controller/web/MyLibraryPageWebController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller.web;
+
+import com.example.demo.service.BookService;
+import com.example.demo.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MyLibraryPageWebController {
+
+    private final BookService bookService;
+
+    @RequestMapping("/web/ukr-lit-exchange/my-library/{id}")
+    public String libraryPage(Model model, @PathVariable Long id){
+        model.addAttribute("userBooks", bookService.getBooksByOwnerId(id));
+        return "my-library-page";
+    }
+}
