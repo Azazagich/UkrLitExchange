7e7c0438d0bb77d636ba8cb349a9b5d7b9754190
Azazagich <s.kovalchuk@innovinnprom.com>
2025-04-01 00:06:38 +0300

    feat(security): set custom session security configuration

---
+++ src/main/java/com/example/demo/controller/web/AuthWebController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller.web;
+
+import com.example.demo.service.RegistrationService;
+import com.example.demo.service.dto.UserRegistrationDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthWebController {
+
+    private final RegistrationService registrationService;
+
+    @GetMapping("/login")
+    public String showLoginForm(){
+        return "login";
+    }
+
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserRegistrationDTO());
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String registerUser(
+            @ModelAttribute("user") @Valid UserRegistrationDTO registrationDto,
+            BindingResult bindingResult,
+            Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        try {
+            registrationService.registerUser(registrationDto);
+            return "redirect:/login";
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            return "registration";
+        }
+    }
+}
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,8 +1,17 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsUsersByEmail(@Email String email);
+
+    boolean existsUserByUsername(String username);
+}
